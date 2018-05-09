4426ba7226749172456df1efbde9c0680cdc23fe
Vishnudev <thevishnudev@gmail.com>
2018-05-09 20:22:05 +0530

    Integratation of frontend and backend

--- frontend/piano_frontend.py
+++ frontend/piano_frontend.py
@@ -1,14 +1,27 @@
+import simpleaudio as sa
+import time
+import random
+import numpy as np
+import cv2
+import keras
+import time
+from threading import Thread
+from keras.models import model_from_json
 try:
     from Tkinter import Tk, Frame, BOTH, Label, PhotoImage
 except ImportError:
     from tkinter import Tk, Frame, BOTH, Label, PhotoImage
-import simpleaudio as sa
-import time
-import random
-from threading import Thread
+
+SIZE_BOUNDING_SQUARE = 300
+FILEPATH ="/home/jubin/Videos/new_data_from_real_time/"
+MODEL_JSON = "model.json"
+MODEL_WEIGHTS = "model.h5"
+VIDEO_FEED = 1
+CLASSNAME = 0
+LINE_WIDTH = 2
 
 ANS='00000'
-
+PREV_ANS=ANS
 KEYS_TO_NOTES = {
     1: 'C1',
     2: 'D1',
@@ -68,12 +81,14 @@ def key_pressed(key):
 
 class Piano(Frame):
     def start(self):
-        global ANS
-        print(ANS)
-        for i in range(len(ANS)):
-            if(ANS[i]=='1'):
-                key_pressed(i+1)
-        self.after(1000,self.start)
+        global ANS, PREV_ANS
+        if PREV_ANS!=ANS:
+            for i in range(len(ANS)):
+                # if(ANS[i]=='1'):
+                if(ANS[i]=='1' and ANS[2]!='1'):
+                    key_pressed(i+1)
+        PREV_ANS=ANS
+        self.after(200,self.start)
     
     def __init__(self, parent):
 
@@ -124,16 +139,117 @@ def main():
     app.after(10,app.start)
     app.mainloop()
 
-def update_ans():
+def real_time_log():
     global ANS
-    for i in range(1000):
-        ANS =str(random.randint(0,1))+str(random.randint(0,1))+str(random.randint(0,1))+str(random.randint(0,1))+str(random.randint(0,1))
-        time.sleep(1)
+    t = time.time()
+    ##########
+    # f = open("model.json", "r")
+    # json_string = f.read()
+    # f.close()
+    ##########
+    ### OR ###
+    ##########
+    with open(MODEL_JSON, "r") as f:
+        json_string = f.read()
+    ##########
+    loaded_model = keras.models.model_from_json(json_string) 
+    loaded_model.load_weights(MODEL_WEIGHTS)
+    loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
+    print("\n"+'*'*40,"\nTRAINED MODEL LOADED IN", time.time() - t, "sec.\n"+'*'*40,"\n")
+
+    START_PREDICTING_FLAG = False
+
+    cap = cv2.VideoCapture(1)
+
+    # for logging the frame rate.
+    # t = time.time()
+    # frame_cnt = 0
+    while True:
+        ret, frame = cap.read()
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # increase the sleep time to decrease the frame rate.
+        ## NO SLEEP - time.sleep(0.0)  - 24 frames/sec
+        ## 0.05 sec - time.sleep(0.05) - 10 frames/sec
+        ## 0.06 sec - time.sleep(0.06) - 9 frames/sec [ BEST - may be ]
+        ## 0.07 sec - time.sleep(0.07) - 8 frames/sec
+        ## 0.08 sec - time.sleep(0.08) - 8 frames/sec
+        ## 0.1 sec  - time.sleep(0.1)  - 7 frames/sec
+        # time.sleep(0.06)
+        
+        # for logging the frame rate.
+        # frame_cnt += 1
+        # cur_time = time.time()
+        # print("Frame Rate =>", int(frame_cnt / (cur_time-t)), "frames/sec")
+
+        # for generating the bounding box and cropping the image
+        mid_width = int(frame.shape[1] / 2)
+        mid_height = int(frame.shape[0] / 2)
+        top_left = ((mid_width-int(SIZE_BOUNDING_SQUARE/2)), (mid_height+30))
+        bottom_right = ((mid_width+int(SIZE_BOUNDING_SQUARE/2)), (mid_height+int(SIZE_BOUNDING_SQUARE/2)))
+        top_left_new = ((mid_width-int(SIZE_BOUNDING_SQUARE/2)), (mid_height-int(SIZE_BOUNDING_SQUARE/2)))
+
+        smallImg = np.array(frame[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]])
+
+        cv2.rectangle(frame,top_left_new,bottom_right,(255,255,255), LINE_WIDTH)
+        cv2.rectangle(frame,top_left,bottom_right,(0,255,0), LINE_WIDTH)
+
+        if START_PREDICTING_FLAG:
+            # cv2.imwrite("/home/jubin/"+str(time.time())+".png", smallImg)
+            # img = cv2.resize(smallImg, (150, 150))
+            x_test = np.array([smallImg])
+            x_test = x_test.reshape(x_test.shape[0], 120, 300, 3)
+            x_test = x_test.astype('float32')
+            x_test /= 255
+            res = loaded_model.predict(x_test)
+            ans = (res>.7).astype(int)
+            ans=''.join([str(x) for x in ans.reshape(-1)[1:]])
+            print(ans)
+            if ANS!=ans:
+                ANS=ans
+
+        # for showing the video feed.
+        cv2.imshow('frame', frame)
+        k = cv2.waitKey(33)
+        # print(k)
+        if k == 27:
+            break
+        elif k == 32:
+            START_PREDICTING_FLAG = not START_PREDICTING_FLAG
+        elif k==48:
+            print(k)
+            # classname = 0
+            CLASSNAME = str(0)
+        elif k==49:
+            # classname = 1
+            CLASSNAME = str(1)
+        elif k==50:
+            # classname = 2
+            CLASSNAME = str(2)
+        elif k==51:
+            # classname = 3
+            CLASSNAME = str(3)
+        elif k==52:
+            # classname = 3
+            CLASSNAME = str(4)
+        elif k==53:
+            # classname = 3
+            CLASSNAME = str(5)
+        elif k==83 or k==115:
+            # for saving the image
+            print("saving to: ", CLASSNAME)
+            name = FILEPATH+CLASSNAME+"/"+str(time.time())+".png"
+            cv2.imwrite(name, smallImg)
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 
 if __name__ == '__main__':
-    t=Thread(target=update_ans,args=())
+    t=Thread(target=real_time_log,args=())
     t.start()
     main()
     # update_ans()
